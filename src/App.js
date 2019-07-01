a9ff74ac6ee39ca2790d91f267badd45d9bf8ded
hanasari <Calvin@Daejongui-iMac.local>
2019-07-01 15:28:55 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    boards: [
+      {
+        brdno: 1,
+        brdwriter: 'Bang Daejong',
+        brdtitle: 'If you intend to live then you die',
+        brddate: new Date()
+      },
+      {
+        brdno: 2,
+        brdwriter: 'So Sino',
+        brdtitle: 'Founder for two countries',
+        brddate: new Date()
+      },
+    ]
+  }
+
+  render(){
+    const {boards} = this.state;
+    const list = boards.map(function(row){
+      return row.brdno + row.brdwriter;
+    });
+
+    return (
+      <div>
+        {list}
+      </div>
+    );
+  }
 }
 
 export default App;
